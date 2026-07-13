26d7366c6c0078d44cec83ad479de5b22974a069
sevilkhojasteh <sevilkhojasteh@gmail.com>
2026-07-13 13:25:32 +0330

    first commit

---
+++ Prerequisite/hello_world.py
@@ -0,0 +1,14 @@
+from tensorflow import tf
+import numpy as np
+
+model = tf.keras.Sequential([
+    tf.keras.Input(shape=(1,)),
+    tf.keras.layers.Dense(units=1)
+])
+
+model.compile(optimizer='sgd', loss='mean_squared_error')
+
+xs = np.array([-1.0, 0.0, 1.0, 2.0, 3.0, 4.0], dtype=float)
+ys = np.array([-3.0, -1.0, 1.0, 3.0, 5.0, 7.0], dtype=float)
+
+model.fit(xs, ys, epochs=500)
