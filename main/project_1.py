ac2ceee13758392286031230b2babda7710d1510
sevilkhojasteh <sevilkhojasteh@gmail.com>
2026-07-23 18:49:02 +0330

    main project starting

--- main/project_1.py
+++ main/project_1.py
@@ -20,4 +20,21 @@ def rapp_power_amplifier(symbols, v_sat = 1.0, p = 2.0):
 
     return symbols_out
 
-# 
+# Encoder layer
+
+class NeuralTransmitter(tf.keras.layers.Layer):
+    def __init__(self, num_symbols, **kwargs):
+        super(NeuralTransmitter, self).__init__(**kwargs)
+        self.num_symbols = num_symbols
+
+    def build(self, input_shape):
+        self.dense1 = tf.keras.layers.Dense(64, activation='relu')
+        self.dense2 = tf.keras.layers.Dense(self.num_symbols * 2, activation= None)
+        super(NeuralTransmitter, self).build(input_shape)
+
+    def call(self, inputs):
+        x = self.dense1(inputs)
+        x = self.dense2(x)
+
+        x = tf.reshape(x, [-1, self.num_symbols, 2])
+        symbols = tf.complex(x[..., 0], x[..., 1])
